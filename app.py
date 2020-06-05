d8a1fd92053936b257b8adcbfc50f302657e2ea9
dung <matdieuhau999@gmail.com>
2020-06-05 23:46:04 +0700

    final

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask,  make_response, jsonify
+import subprocess
+import time
+
+app = Flask(__name__)
+
+
+class Process:
+    cmd = ""
+    cpu = ""
+    mem = ""
+    time = ""
+    user = ""
+    pid = ""
+
+    def __init__(self, _cmd, _cpu, _mem, _user, _pid, _time):
+        self.cmd = _cmd
+        self.cpu = _cpu
+        self.mem = _mem
+        self.user = _user
+        self.pid = _pid
+        self.time = _time
+
+
+@app.route("/process",methods=["GET"])
+def process():
+    print "process"
+    process = subprocess.check_output(["ps","-eo","cmd,%cpu,%mem","--sort=-%cpu "]).rstrip("\n").replace("'","").replace(" - ","   ").split("\n")[0:20]
+    res = make_response(jsonify(process),200)
+    return {'time': time.time()}
+
+@app.route("/cpu")
+def getSortByCpu():
+    print "cpu"
+
+    
+    process = subprocess.check_output(["ps","-eo","cmd,%cpu,%mem,user,pid,time" , "--sort=-%cpu"]).rstrip("\n").replace("'","").replace(" - ","   ").split("\n")[1:]
+    data = []
+    
+    for p in process:
+        
+        data.append( dict(cmd= p[0:27], cpu = p[28:32], mem = p[33:37], user = p[38:46], pid = p[47:52], time = p[53:61] )  )
+
+    
+    return {'item' : data}
+
+@app.route("/mem")
+def getSortByMem():
+
+    print "mem"
+
+    process = subprocess.check_output(["ps","-eo","cmd,%cpu,%mem,user,pid,time" , "--sort=-%mem"]).rstrip("\n").replace("'","").replace(" - ","   ").split("\n")[1:]
+    data = []
+    
+    for p in process:
+        
+        data.append( dict(cmd= p[0:27], cpu = p[28:32], mem = p[33:37], user = p[38:46], pid = p[47:52], time = p[53:61] )  )
+    
+    
+    return {'item' : data}
+
+@app.route("/pid")
+def getSortByPid():
+
+    print "pid"
+
+    process = subprocess.check_output(["ps","-eo","cmd,%cpu,%mem,user,pid,time" , "--sort=pid"]).rstrip("\n").replace("'","").replace(" - ","   ").split("\n")[1:]
+    data = []
+
+    for p in process:
+
+        data.append( dict(cmd= p[0:27], cpu = p[28:32], mem = p[33:37], user = p[38:46], pid = p[47:52], time = p[53:61] )  )
+    
+
+    return {'item' : data}
+
+
+
+if __name__ == "__main__":
+    app.run()
--- src/component/Process.js
+++ src/component/Process.js
@@ -1,20 +1,168 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { authPost, authGet } from '../api';
+import { List } from '@material-ui/core/List';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Button } from '@material-ui/core';
 
+
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
 
 function Process(props){
-    useEffect (() => (
-        authGet("/test").then(
-            (res) => {
-                console.log("res",res);
-            }
-        )
-    ))
+
+    const [listProcess, setListProcess] = useState([]);
+    const [count,setCount] = useState(0);
+    const [time, setTime] = useState(0);
+    const classes = useStyles();
+    const [sort, setSort] = useState("cpu");
+    // useEffect (() => (
+
+
+        // fetch('/time').then(
+        //     res => res.json().then(
+        //         data =>{
+
+
+        //             console.log('data', data);
+        //             setListProcess(data.item);
+        //         }
+        //     )
+        // )
+
+    // ),[]);
+
+    useEffect(() => {
+        //console.log("111");
+
+
+        console.log("sort ", sort);
+
+        if(sort == "cpu"){
+            fetch('/cpu').then(
+                res => res.json().then(
+                    data =>{
+
+
+                        console.log('data', data);
+                        setListProcess([]);
+                        setListProcess(data.item);
+                    }
+                )
+            )
+        }else if (sort == "mem"){
+            fetch('/mem').then(
+                res => res.json().then(
+                    data =>{
+
+
+                        console.log('data', data);
+                        setListProcess([]);
+                        setListProcess(data.item);
+                    }
+                )
+            )
+        }else{
+            fetch('/pid').then(
+                res => res.json().then(
+                    data =>{
+
+
+                        console.log('data', data);
+                        setListProcess([]);
+                        setListProcess(data.item);
+                    }
+                )
+            )
+        }
 
 
 
 
-    return (<div> this is my process</div>)
+
+
+        const id = setInterval(() => {
+          setCount(count + 1)
+        }, 2000)
+        return () => clearInterval(id)
+    }, [count])
+
+    const handlePidChange = () => {
+        setSort("pid");
+    }
+
+
+    const handleCpuChange = () =>{
+        setSort("cpu");
+        //window.location.reload();
+    }
+
+    const handleMemChange = () =>{
+        setSort("mem");
+        //window.location.reload();
+    }
+
+    return (
+        <div>
+            <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                <TableRow>
+                    <TableCell>COMMAND</TableCell>
+                    <TableCell align="center">USER</TableCell>
+                    <TableCell align="center">
+                        <Button onClick={handlePidChange} >
+                            %PID
+                        </Button>
+
+
+                    </TableCell>
+
+                    <TableCell align="center">
+                        <Button onClick={handleCpuChange} >
+                            %CPU
+                        </Button>
+
+
+                    </TableCell>
+
+                    <TableCell align="center">
+                        <Button onClick={handleMemChange} >
+                            %MEM
+                        </Button>
+
+
+                    </TableCell>
+                    <TableCell align="center">TIME</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {listProcess.map((row) => (
+                    <TableRow key={row.cmd}>
+                        <TableCell component="th" scope="row">
+                            {row.cmd}
+                        </TableCell>
+                        <TableCell align="center">{row.user}</TableCell>
+                        <TableCell align="center">{row.pid}</TableCell>
+                        <TableCell align="center">{row.cpu}</TableCell>
+                        <TableCell align="center">{row.mem}</TableCell>
+                        <TableCell align="center">{row.time}</TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+            </TableContainer>
+        </div>
+    )
 }
 
 export default Process;
